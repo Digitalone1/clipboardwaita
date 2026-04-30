99c74401f2b61db90978d8da71cca2f8f077125e
Digitalone1 <kurmikon@yahoo.com>
2026-04-30 09:03:30 +0200

    to_string util: avoid static storage of the buffer

--- src/util.hpp
+++ src/util.hpp
@@ -64,10 +64,12 @@ auto to_string(const T num, const std::string &def = "0") -> std::string {
   static constexpr auto max = std::numeric_limits<T>::digits10 +
                               std::numeric_limits<T>::max_digits10 + 10;
 
-  static std::array<char, max> buffer;
+  // Avoid static storage on the buffer since we do not want to access to the
+  // same buffer from different threads, even if it's unlikely to happen.
+  std::array<char, max> buffer;
 
-  static constexpr auto p_init = buffer.data();
-  static constexpr auto p_end = p_init + max;
+  const auto p_init = buffer.data();
+  const auto p_end = p_init + max;
 
   const auto result = std::to_chars(p_init, p_end, num);
 
